a198c3acd2a69cc84eb317216ca83637d825dff6
chloclicot <chloe.v.33@outlook.fr>
2024-11-02 23:42:33 +0100

    fix eureka add gateway

--- eureka-server/src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.gatewayservice;
+package org.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
--- gateway-service/src/main/java/org/example/testservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/example/testservice/GatewayServiceApplication.java
@@ -1,9 +1,11 @@
-package org.example.gatewayservice;
+package org.example.testservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
