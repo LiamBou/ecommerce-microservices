20eae5bb0d382aa8d0102a3a0c79651c602d6b2c
chloclicot <chloe.v.33@outlook.fr>
2024-11-04 00:58:05 +0100

    fix eureka connection

--- product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.productservice.dto;
-
-import java.math.BigDecimal;
-
-public record ProductRequest(String id, String name, String race, String color, BigDecimal price, String description, String image) {
-}
--- product-service/src/main/java/com/example/productservice/dto/ProductResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.productservice.dto;
-
-import java.math.BigDecimal;
-
-public record ProductResponse(String id, String name, String race, String color, BigDecimal price, String description, String image) {
-
-}
