f6157bd8d14b1601a5815ad98d0618ead19ce5e5
chloclicot <chloe.v.33@outlook.fr>
2024-11-04 21:52:49 +0100

    add keycloak server

---
+++ gateway-service/src/main/java/org/example/testservice/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package org.example.testservice.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.OAuth2ResourceServerDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(
+                                "/eureka/**"
+                        ).permitAll()// Allow access to these endpoints
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));// Disable CSRF for API requests
+
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+    }
+}
+
